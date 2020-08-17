9a7252de1e243142a2afcc733259ac3f2659e78d
Vicky Yin <v-vyin@microsoft.com>
2020-08-17 15:39:02 +0800

    first commit

---
+++ CompareTable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CompareTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step1. 從182取出table 放入#tabl
+            //Step1.1 連線到225.17
+            //Step1.2 取出相對應table放入#table
+            //Step2. #table跟225.17中比較
+            //Step2.1 compare 兩個table
+            //Step2.2 將結果發Email
+            //Step3. 覆蓋
+            //Step3.1 #table覆蓋SKHDBA中的table
+        }
+    }
+}
